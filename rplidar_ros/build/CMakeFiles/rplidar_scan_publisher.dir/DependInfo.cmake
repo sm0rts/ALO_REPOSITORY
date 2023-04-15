91bf848d6d0f94e430ebd5a5548ce8c3145b3b82
ALO_Desktop <estereonos@gmail.com>
2023-04-15 12:26:10 +0100

    added the rplidar library properly?

---
+++ rplidar_ros/build/CMakeFiles/rplidar_scan_publisher.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/sdk/src/arch/linux/net_serial.cpp" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/arch/linux/net_serial.cpp.o" "gcc" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/arch/linux/net_serial.cpp.o.d"
+  "/home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/sdk/src/arch/linux/net_socket.cpp" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/arch/linux/net_socket.cpp.o" "gcc" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/arch/linux/net_socket.cpp.o.d"
+  "/home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/sdk/src/arch/linux/timer.cpp" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/arch/linux/timer.cpp.o" "gcc" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/arch/linux/timer.cpp.o.d"
+  "/home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/sdk/src/hal/thread.cpp" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/hal/thread.cpp.o" "gcc" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/hal/thread.cpp.o.d"
+  "/home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/sdk/src/rplidar_driver.cpp" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/rplidar_driver.cpp.o" "gcc" "CMakeFiles/rplidar_scan_publisher.dir/sdk/src/rplidar_driver.cpp.o.d"
+  "/home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/src/rplidar_scan_publisher.cpp" "CMakeFiles/rplidar_scan_publisher.dir/src/rplidar_scan_publisher.cpp.o" "gcc" "CMakeFiles/rplidar_scan_publisher.dir/src/rplidar_scan_publisher.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rplidar_ros/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros
+# Build directory: /home/alo-desktop/ALO_ROBOT_PROJECT/src/rplidar_ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
